efcba51da791ffd8b42fe733510309df5a608e63
Samuel Sciolla <ssciolla@umich.edu>
2023-09-27 18:19:53 -0400

    Add more explanation in comments

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,4 +1,9 @@
-# https://stackoverflow.com/questions/683989/how-do-you-deal-with-the-conflict-between-activesupportjson-and-the-json-gem
+# Patch ActiveSupport to_json implementation so it uses the json gem version of the method
+# that is expected by the Sinatra backend.
+
+# Resources
+# - https://stackoverflow.com/questions/683989/how-do-you-deal-with-the-conflict-between-activesupportjson-and-the-json-gem
+# - https://github.com/rails/rails/blob/v5.2.8/activesupport/lib/active_support/core_ext/object/json.rb
 
 module ActiveSupport
   module ToJsonWithActiveSupportEncoder
