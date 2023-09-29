ecb544b7ef4fe9150212322f7a2013608e6e711d
Samuel Sciolla <ssciolla@umich.edu>
2023-09-29 15:30:12 -0400

    Add some more explanatory comments

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -2,6 +2,11 @@
 # that is expected by the Sinatra backend. This patch counteracts the effect of ActiveSupport
 # which was introduced by multiple gems needed by this plugin.
 
+# If you do not have this patch, at least one endpoint in the backend (/reports) will have changed output
+# that will break a template in the frontend.
+# To reproduce the issue, when logged in with a user with repository access,
+# visit http://localhost:8080/jobs/new?job_type=report_job
+
 # Resources
 # - https://stackoverflow.com/questions/683989/how-do-you-deal-with-the-conflict-between-activesupportjson-and-the-json-gem
 # - https://github.com/rails/rails/blob/v5.2.8/activesupport/lib/active_support/core_ext/object/json.rb
--- spec/backend_plugin_init_spec.rb
+++ spec/backend_plugin_init_spec.rb
@@ -1,18 +1,14 @@
 require "date"
-require "logger"
 
-require "json"
-require "active_support/all"
+require "json"  # Makes to_json work as expected
+require "active_support/all"  # Introduces breaking behavior
 
 require_relative "../backend/plugin_init.rb"
-
-logger = Logger.new($stdout)
 
 RSpec.describe do
   context "with plugin initialized in backend" do
     it "serializes class references in hashes to string names" do
       test_hash = {test_date: Date}
-      logger.info test_hash.method(:to_json)
       expect(test_hash.to_json).to eq('{"test_date":"Date"}')
     end
   end
