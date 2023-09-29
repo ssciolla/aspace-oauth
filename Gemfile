e0e8519f31b79deaa2561abc3db5eee4e1cd1f6d
Samuel Sciolla <ssciolla@umich.edu>
2023-09-29 14:07:31 -0400

    Add a comment to Gemfile; tweak spec strings

--- Gemfile
+++ Gemfile
@@ -13,8 +13,11 @@ gem 'addressable',   '~> 2.8'
 # Pinning this resolves undefined method `[]' for #<OpenSSL::ASN1::Sequence:0x4bb0df3c>
 gem "json-jwt", "1.13.0"
 
-group :test do
+group :test, optional: true do
+  # ArchivesSpace has json installed in the backend:
+  # https://github.com/archivesspace/archivesspace/blob/90195216bdf614e1335f869144fc485ae9e8c002/backend/Gemfile#L11
   gem "json"
+
   gem "rspec"
   gem "rspec-core"
 end
--- spec/backend_plugin_init_spec.rb
+++ spec/backend_plugin_init_spec.rb
@@ -9,12 +9,11 @@ require_relative "../backend/plugin_init.rb"
 logger = Logger.new($stdout)
 
 RSpec.describe do
-  context "with plugin initialized" do
-    it "serializes class references in hashes to strings" do
+  context "with plugin initialized in backend" do
+    it "serializes class references in hashes to string names" do
       test_hash = {test_date: Date}
       logger.info test_hash.method(:to_json)
       expect(test_hash.to_json).to eq('{"test_date":"Date"}')
     end
   end
 end
-
